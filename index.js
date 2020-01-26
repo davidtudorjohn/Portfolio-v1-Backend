18a0e0f083ac38f5d4f0291d9aad6a1a2302c1a9
David John <davidjohn_atl@yahoo.com>
2020-01-26 16:28:20 -0500

    added middleware for CORS

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 const port = 5000;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  // if (res.header === "OPTIONS") {
+  //   res.header(
+  //     "Access-Control-Allow-Methods",
+  //     "PUT",
+  //     "POST",
+  //     "PATCH",
+  //     "DELETE"
+  //   );
+  //   return res.status(200).json({});
+  // }
+  next();
+});
+
 mongoose.connect(
   process.env.ATLAS,
   { useNewUrlParser: true, useUnifiedTopology: true },
